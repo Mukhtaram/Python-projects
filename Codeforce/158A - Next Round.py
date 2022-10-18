93779fa11dca3e63d7c7bd79e91496d35607ad7f
mukhtaram.sulaiman <mukhtaram.sulaimanov_2024@ucentralsia.org>
2022-10-18 11:25:10 +0600

    problem from codeforces

--- Codeforce/158A - Next Round.py
+++ Codeforce/158A - Next Round.py
@@ -1,10 +1,12 @@
-import sys
-
-m, k = map(int, sys.stdin.readline().split())
-lst = list(map(int, input().split()))
+a,b = map(int, input().split())
+c = list(map(int,input().split()))
 q = 0
 
-for student in lst:
-    if student >= lst[k] and student > 0:
-        q += 1
+for i in range(a):
+    if c[i] == c[b-1] and c[b-1] == 0:
+        q+=0
+    elif c[i] >= c[b-1]:
+        q+=1
+    else:
+        q+=0
 print(q)
